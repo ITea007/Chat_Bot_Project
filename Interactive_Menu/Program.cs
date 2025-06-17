a5265a24ac8295d14b4a5ae708ebab9a45b70f3b
ITea007 <i.s.shevchenko@gmail.com>
2025-06-17 18:04:49 +0300

    Refactoring. List -> Dictionary.

--- Interactive_Menu/Program.cs
+++ Interactive_Menu/Program.cs
@@ -8,23 +8,37 @@ namespace Interactive_Menu
         /// <summary>
         /// Текущее имя пользователя
         /// </summary>
-        private static string UserName = "";
+        private static string _userName;
         /// <summary>
-        /// Доступные пользователю команды. Список доступных комманд.
+        /// Доступные пользователю команды
         /// </summary>
-        private static readonly List<string> AvailableCommands = ["/echo", "/start", "/help", "/info", "/exit", "/addtask", "/showtasks", "/removetask"];
+        private static readonly Dictionary<int, string> _availableCommandsDict;
         /// <summary>
         /// Индекс команды "/echo" в списке доступных команд
         /// </summary>
-        private static readonly int EchoCommandIndex = 0;
+        private static readonly int _echoCommandIndex;
         /// <summary>
         /// Список текущих задач
         /// </summary>
-        private static List<string> Tasks = new List<string>();
+        private static List<string> _tasks;
 
+        /// <summary>
+        /// Инициализация статических полей программы
+        /// </summary>
+        static Program()
+        {
+            _userName = "";
+            _availableCommandsDict = new Dictionary<int, string>()
+            {
+                { 0, "/echo" }, { 1, "/start" }, { 2, "/help" },
+                { 3, "/info" }, { 4, "/exit" }, { 5, "/addtask"},
+                { 6, "/showtasks"}, { 7, "/removetask"}
+            };
+            _echoCommandIndex = _availableCommandsDict.Where(i => i.Value.Equals("/echo")).FirstOrDefault().Key;
+            _tasks = new List<string>();
+        }
 
         #region CommandsRealization
-
 
         /// <summary>
         /// Реализация команды "/start"
@@ -63,7 +77,10 @@ namespace Interactive_Menu
         /// </summary>
         static void InfoCommand()
         {
-            Console.WriteLine(  "*  Текущая версия программы 2.0.  Дата создания 16-06-2025\r\n" +
+            Console.WriteLine("\r\n" +
+                                "*  Текущая версия программы 3.0.  Дата создания 17-06-2025\r\n" +
+                                "   Добавлено ...\r\n" +
+                                "*  Текущая версия программы 2.0.  Дата создания 16-06-2025\r\n" +
                                 "   Добавлены команды /addtask, /showtasks, /removetask\r\n" +
                                 "*  Версия программы 1.0.  Дата создания 25-05-2025\r\n" +
                                 "   Реализованы команды /start, /help, /info, /echo, /exit\r\n");
@@ -83,15 +100,15 @@ namespace Interactive_Menu
         /// лишние пробелы вначале, выводится на консоль. 
         /// </summary>
         /// <param name="inputString">Входящая строка</param>
-        /// <param name="availableCommandsList">Список доступных комманд</param>
+        /// <param name="availableCommandsDict">Словарь доступных комманд</param>
         /// <param name="echoCommandIndex">Индекс команды "/echo" в списке доступных команд</param>
-        static void EchoCommand(string inputString, List<string> availableCommandsList, int echoCommandIndex, string userName)
+        static void EchoCommand(string inputString, Dictionary<int, string> availableCommandsDict, int echoCommandIndex, string userName)
         {
             if (!string.IsNullOrEmpty(inputString) &&
                 !string.IsNullOrEmpty(userName) && 
-                inputString.StartsWith(availableCommandsList[echoCommandIndex]))
+                inputString.StartsWith(availableCommandsDict[echoCommandIndex]))
             {
-                var subString = inputString.Substring(availableCommandsList[echoCommandIndex].Length);
+                string subString = inputString.Substring(availableCommandsDict[echoCommandIndex].Length);
                 Console.WriteLine(subString.TrimStart());
             }
         }
@@ -156,45 +173,34 @@ namespace Interactive_Menu
         #endregion
 
         /// <summary>
-        /// Метод определяет, является ли входящая строка командой, и возвращает порядковый номер введеной команды из списка доступных команд. 
+        /// Метод определяет, является ли входящая строка командой, и возвращает порядковый номер введеной команды из словаря доступных команд. 
         /// </summary>
         /// <param name="inputString"></param>
-        /// <param name="availableCommandsList">Список доступных команд</param>
-        /// <param name="echoCommandIndex">Индекс команды "/echo" в списке доступных команд</param>
-        /// <returns>Возвращает индекс команды из списка доступных команд, -1 если не команда найдена</returns>
-        static int DefineCommandNum(string inputString, List<string> availableCommandsList, int echoCommandIndex)
+        /// <param name="availableCommandsDict">Словарь доступных команд</param>
+        /// <returns>Возвращает индекс команды из списка доступных команд</returns>
+        static int DefineCommandNum(string inputString, Dictionary<int, string> availableCommandsDict)
         {
-            for (int i = 0; i < availableCommandsList.Count; i++)
-            {
-                if (string.Equals(inputString, availableCommandsList[i]) ||
-                        (!string.IsNullOrEmpty(inputString) &&
-                        inputString.StartsWith(availableCommandsList[i]) &&
-                        (i == echoCommandIndex)))
-                    return i;
-            }
-            return -1;
+            return availableCommandsDict.Where(i => i.Value.Equals(inputString)).FirstOrDefault().Key;
         }
 
-
         /// <summary>
-        /// Метод выполняет вызов команды по её порядковому номеру из списка доступных команд.
+        /// Метод выполняет вызов команды по её порядковому номеру из словаря доступных команд.
         /// </summary>
         /// <param name="commandNum">Порядковый номер команды из списка доступных команд</param>
         /// <param name="inputString">Входящая строка</param>
-        /// <param name="availableCommandsList">Список доступных команд</param>
+        /// <param name="availableCommandsDict">Словарь доступных команд</param>
         /// <param name="echoCommandIndex">Индекс команды "/echo" в списке доступных команд</param>
         /// <param name="userName">Текущее имя пользователя</param>
         /// <param name="tasksList">Список текущих задач</param>
-        static void ExecuteCommand(int commandNum, string inputString, List<string> availableCommandsList,
+        static void ExecuteCommand(int commandNum, string inputString, Dictionary<int, string> availableCommandsDict,
                                    int echoCommandIndex, ref string userName, List<string> tasksList)
         {
             if (commandNum != echoCommandIndex)
                 PrintUserName(userName);
             switch (commandNum)
             {
-                
                 case 0:
-                    EchoCommand(inputString, availableCommandsList, echoCommandIndex, userName);
+                    EchoCommand(inputString, availableCommandsDict, echoCommandIndex, userName);
                     return;
                 case 1:
                     StartCommand(ref userName);
@@ -217,29 +223,30 @@ namespace Interactive_Menu
                 case 7:
                     RemoveTask(tasksList);
                     return;
-                case -1:
-                    return;
             }
         }
 
         /// <summary>
-        /// Метод выводит в консоль строку со всеми доступными командами. Если задано имя пользователя, то к выводу добавляется команда 
-        /// "/echo".
+        /// Метод выводит в консоль строку со всеми доступными командами. Если задано имя пользователя, то к выводу добавляется команда "/echo".
         /// </summary>
-        /// <param name="availableCommandsList">Список доступных команд</param>
+        /// <param name="availableCommandsDict">Словарь доступных команд</param>
         /// <param name="echoCommandIndex">Индекс команды "/echo" в списке доступных команд</param>
         /// <param name="userName">Текущее имя пользователя</param>
-        static void PrintCommands(List<string> availableCommandsList, int echoCommandIndex, string userName)
+        static void PrintCommands(Dictionary<int, string> availableCommandsDict, int echoCommandIndex, string userName)
         {
             Console.Write("Вам доступны команды: ");
-            for (int i = 1; i < availableCommandsList.Count; i++)
+
+            foreach (var item in availableCommandsDict)
             {
-                if (i > 1) 
-                    Console.Write(", ");
-                Console.Write(availableCommandsList[i]);
+                if (item.Key != echoCommandIndex)
+                {
+                    if (item.Key > 1)
+                        Console.Write(", ");
+                    Console.Write(item.Value);
+                }
             }
             if (!string.IsNullOrEmpty(userName))
-                Console.WriteLine($", {availableCommandsList[echoCommandIndex]}");
+                Console.WriteLine($", {availableCommandsDict[echoCommandIndex]}");
             else
                 Console.WriteLine();
         }
@@ -266,15 +273,14 @@ namespace Interactive_Menu
             Console.Write("Добро пожаловать! ");
             do
             {
-                PrintUserName(UserName);
-                PrintCommands(AvailableCommands, EchoCommandIndex, UserName);
+                PrintUserName(_userName);
+                PrintCommands(_availableCommandsDict, _echoCommandIndex, _userName);
 
                 var inputString = Console.ReadLine();
                 if (!string.IsNullOrEmpty(inputString))
                 {
-                    var commandNum = DefineCommandNum(inputString, AvailableCommands, EchoCommandIndex);
-                    if (commandNum >= 0)
-                        ExecuteCommand(commandNum, inputString, AvailableCommands, EchoCommandIndex, ref UserName, Tasks);
+                    int commandNum = DefineCommandNum(inputString, _availableCommandsDict);
+                    ExecuteCommand(commandNum, inputString, _availableCommandsDict, _echoCommandIndex, ref _userName, _tasks);
                 }
             }
             while (true);
