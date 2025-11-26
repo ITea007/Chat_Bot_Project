f53160c587652fd268a3384f5ecfc4fcf6b67d1b
ITea007 <i.s.shevchenko@gmail.com>
2025-11-26 12:48:16 +0300

    16.1

---
+++ Interactive_Menu/BackgroundTasks/IBackgroundTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Menu.BackgroundTasks
+{
+    public interface IBackgroundTask
+    {
+        Task Start(CancellationToken ct);
+    }
+}
