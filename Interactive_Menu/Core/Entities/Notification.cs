e658e6e120b76ca1ce8e54ed3834915967c2526f
ITea007 <i.s.shevchenko@gmail.com>
2025-11-28 21:21:34 +0300

    Class Notification

---
+++ Interactive_Menu/Core/Entities/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Menu.Core.Entities
+{
+    internal class Notification
+    {
+        Guid Id;
+        ToDoUser User;
+        string Type; //Тип нотификации. Например: DeadLine_{ToDoItem.Id}, Today_{DateOnly.FromDateTime(DateTime.UtcNow)}
+        string Text; //Текст, который будет отправлен
+        DateTime ScheduledAt; //Запланированная дата отправки
+        bool IsNotified; //Флаг отправки
+        DateTime? NotifiedAt; //Фактическая дата отправки
+    }
+}
